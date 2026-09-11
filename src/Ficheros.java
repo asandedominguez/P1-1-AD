56a5db0e9c37d67c65074be4bbd0bb7a0ee55e3d
Adrián <asandedominguez@danielcastelao.org>
2026-09-11 14:13:40 +0200

    Primeros 3 ejercicios

---
+++ src/Ficheros.java
@@ -0,0 +1,34 @@
+import java.io.File;
+public class Ficheros {
+    public static String eDirectorio(String directorio) {
+        File ruta = new File(directorio);
+        if (ruta.isDirectory()) {
+            return "é directorio";
+        }
+        else {
+            return "non é directorio";
+        }
+    }
+
+    public static String eFicheiro(String dir) {
+       File ruta2 = new File(dir);
+       if (ruta2.isFile()) {
+           return "É ficheiro";
+       }
+       else {
+           return "Non é ficheiro";
+       }
+    }
+
+    public static String creaDirectorio(String crear) {
+        File ruta3 = new File(crear);
+        if (ruta3.exists()) {
+            return "O directorio xa existe";
+        }
+        if (ruta3.mkdir()) {
+            return "Creado correctamente";
+        } else {
+            return "Non se puido crear";
+        }
+    }
+}
