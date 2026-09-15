f33e6ff667eb76a5fc6e4fa38bf0ec1790b0f44c
Adrián <asandedominguez@danielcastelao.org>
2026-09-15 12:34:14 +0200

    Parte 1 echa

--- src/Ficheros.java
+++ src/Ficheros.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Ficheros {
     public static String eDirectorio(String directorio) {
@@ -50,4 +52,74 @@ public class Ficheros {
         }
         return "";
     }
+
+    public static String modoAcceso(String dirName, String fileName) {
+        File fichero2 = new File(dirName, fileName);
+        if (fichero2.canWrite()) {
+            System.out.println("Escritura si");
+        }
+        if (!fichero2.canWrite()) {
+            System.out.println("Escritura no");
+        }
+        if (fichero2.canRead()) {
+            System.out.println("Lectura si");
+        }
+        if (!fichero2.canRead()) {
+            System.out.println("Lectura no");
+        }
+        return "";
+    }
+
+    public static String calcularLonxitude(String dirName, String fileName) {
+        File fichero3 = new File (dirName, fileName);
+        System.out.println(fichero3.length()); ;
+        return "";
+    }
+
+    public static String mLectura(String dirName, String fileName) {
+        File fichero3 = new File (dirName, fileName);
+        if (fichero3.exists()) {
+            if (fichero3.setReadOnly()) {
+                return "Creado con exito";
+            }
+        }
+        return "No existe";
+    }
+
+    public static String mEscritura(String dirName, String fileName) {
+        File fichero4 = new File (dirName, fileName);
+        if (fichero4.exists()) {
+            if (fichero4.setWritable(true)) {
+                return "Se escribe correctamente";
+            }
+            return "No se puede escribir";
+        }
+        return "No existe";
+    }
+
+    public static String borrarFichero(String dirName, String fileName) {
+        File fichero5 = new File (dirName, fileName);
+        if (fichero5.exists()) {
+            fichero5.delete();
+            return "Borrado correctamente";
+        }
+        return "No existe";
+    }
+
+    public static String borraDirectorio(String dirName) {
+        File fichero6 = new File (dirName);
+        if (fichero6.exists()) {
+            fichero6.delete();
+            return "Borrado con exito";
+        }
+        return "Ruta inexistente o con descendencia";
+    }
+
+    public static String mContido(String dirName) {
+        File fichero7 = new File (dirName);
+        System.out.println(Arrays.toString(fichero7.list()));
+        return "";
+    }
+
+
 }
